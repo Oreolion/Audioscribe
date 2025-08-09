417bd5963af590789e41e84a9be259061d5c9ca7
REMY ADEDEJI  #RA <remyoreo11@gmail.com>
2025-08-09 03:18:15 -0700

    add minor fix;

--- app/api/webhooks/stripe/route.ts
+++ app/api/webhooks/stripe/route.ts
@@ -6,7 +6,7 @@ import { PaymentStatus } from '@prisma/client';
 
 // Initialize Stripe with a specific API version
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2022-11-15',
+  apiVersion: '2024-11-20.acacia',
 });
 
 const paymentService = new PaymentService();
